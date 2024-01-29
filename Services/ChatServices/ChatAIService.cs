694fa64eddbd008cfcfc759efd2987ace5975043
Yab <yabuman46@gmail.com>
2024-01-29 13:34:15 +0300

    feat: working on llm integration

--- Services/ChatServices/ChatAIService.cs
+++ Services/ChatServices/ChatAIService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.Extensions.Options;
@@ -16,14 +17,14 @@ namespace UserAuthentication.Services.ChatServices
         private readonly IMongoCollection<Message> _messageCollection;
         private readonly IMapper _mapper;
 
-        public ChatAIService(IOptions<MongoDBSettings> options, IMapper mapper): base(options)
+        public ChatAIService(IOptions<MongoDBSettings> options, IMapper mapper) : base(options)
         {
             _messageCollection = GetCollection<Message>("Messages");
             _mapper = mapper;
         }
         private async Task<(int, string?, UsageMessageDTO?)> AddMessage(string userId, string message, MessageType messageType = MessageType.Human)
         {
-            Message newMessage = new(){UserId = userId, Type = messageType, Content = message};
+            Message newMessage = new() { UserId = userId, Type = messageType, Content = message };
             var createdMessage = _mapper.Map<UsageMessageDTO>(newMessage);
             try
             {
@@ -32,54 +33,63 @@ namespace UserAuthentication.Services.ChatServices
             }
             catch (Exception ex)
             {
-                
+
                 return (0, ex.Message, null);
             }
         }
 
-        // private async Task<(int, string?, string?)> HttpRequestToServer()
-        // {
-        //     using (HttpClient httpClient = new HttpClient())
-        //     {
-        //         // Specify the API endpoint URL
-        //         string apiUrl = "http://localhost:5000/ask";
+        private async Task<(int, string?, string?)> HttpRequestToServer(string question)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // Specify the API endpoint URL
+                string apiUrl = "http://localhost:5000/ask";
 
-        //         try
-        //         {
-        //             HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+                try
+                {
+                    var requestData = new
+                    {
+                        query = question
+                    };
 
-        //             if (response.IsSuccessStatusCode)
-        //             {
-        //                 string responseData = await response.Content.ReadAsStringAsync();
+                    string jsonBody = JsonSerializer.Serialize(requestData);
 
-        //                 Console.WriteLine("API Response: " + responseData);
-        //             }
-        //             else
-        //             {
-        //                 Console.WriteLine($"API Request Failed: {response.StatusCode} - {response.ReasonPhrase}");
-        //             }
-        //         }
-        //         catch (Exception ex)
-        //         {
-        //             return (0, ex.Message, null);
-        //         }
-        //     }
-        // }
+                    StringContent content = new(jsonBody, System.Text.Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseData = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine("API Response: " + responseData);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"API Request Failed: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                    return (1, "Message success", null);
+                }
+                catch (Exception ex)
+                {
+                    return (0, ex.Message, null);
+                }
+            }
+        }
 
         public async Task<(int, string?, UsageMessageDTO?)> AskAI(string userId, string message)
         {
             try
             {
-               //TODO: Ask llm for response
+                //TODO: Ask llm for response
 
-               var addUserMessage = Task.Run(() => AddMessage(userId, message));
-               var addAiMessage = Task.Run(() => AddMessage(userId, message, MessageType.AI));
-               await Task.WhenAll(addUserMessage, addAiMessage);
-               return (1, "Asking llm successful", null);
+                var addUserMessage = Task.Run(() => AddMessage(userId, message));
+                var addAiMessage = Task.Run(() => AddMessage(userId, message, MessageType.AI));
+                await Task.WhenAll(addUserMessage, addAiMessage);
+                await HttpRequestToServer("What am I sick about?");
+                return (1, "Asking llm successful", null);
             }
             catch (Exception ex)
             {
-                
+
                 return (0, ex.Message, null);
             }
         }
