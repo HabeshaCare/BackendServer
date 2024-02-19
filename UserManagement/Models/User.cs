fa359608c5883a74cb1c3be59545ef3dac6e6058
Yab <yabuman46@gmail.com>
2024-02-19 16:07:57 +0300

    refactor: Updated to match documentation

--- UserManagement/Models/User.cs
+++ UserManagement/Models/User.cs
@@ -13,28 +13,17 @@ namespace UserManagement.Models
 {
     public class User
     {
-        [EmailAddress(ErrorMessage = "Invalid email address")]
-        public required string Email { get; set; }
-
-        [Phone(ErrorMessage = "Invalid phone number format")]
-        public string? Phonenumber { get; set; }
-
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
-
-        [Required(ErrorMessage = "This is a required field")]
-        public string? Profession { get; set; }
-
-        public string? Fullname { get; set; } = "";
-
+        public string? Fullname { get; set; }
+        [Phone(ErrorMessage = "Invalid phone number format")]
+        public string? Phonenumber { get; set; }
+        public string? Email { get; set; }
         public string? Gender { get; set; }
-
         public string? City { get; set; }
-
         [Range(1, 150)]
         public int? Age { get; set; }
-
         public string? ImageUrl { get; set; }
 
         [JsonConverter(typeof(StringEnumConverter))]
