b9feba60a63f95204fe1e06a42c37f32eeac88d0
Yab <yabuman46@gmail.com>
2024-02-19 15:35:50 +0300

    feat: Added BSON representation to admin

--- UserManagement/Models/Adminsitrator.cs
+++ UserManagement/Models/Adminsitrator.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace UserManagement.Models
 {
     public class Administrator : User
     {
-        // public new AdminRole Role { get; set; }
+        [BsonRepresentation(BsonType.String)]
+        public AdminRole AdminRole { get; set; }
     }
 }
