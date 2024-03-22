5f3bf7c14250e980750ad9923b5c7ba43a16bdeb
Yab <yabuman46@gmail.com>
2024-03-22 10:16:01 +0300

    feat: Updated Admin interface to conform to class

--- UserManagement/Services/UserServices/AdminService/IAdminService.cs
+++ UserManagement/Services/UserServices/AdminService/IAdminService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserManagement.DTOs;
 using UserManagement.DTOs.AdminDTOs;
 using UserManagement.Models;
 
@@ -11,8 +12,8 @@ namespace UserManagement.Services.UserServices
     {
         Task<SResponseDTO<UsageAdminDTO>> GetAdminById(string adminId);
         Task<SResponseDTO<UsageAdminDTO>> GetAdminByEmail(string adminEmail);
-        Task<(int, string, Administrator?)> AddAdmin(Administrator admin);
-        Task<(int, string, Administrator?)> UpdateAdmin(UpdateAdminDTO adminDTO, string id);
+        Task<SResponseDTO<Administrator>> AddAdmin(Administrator admin);
+        Task<SResponseDTO<Administrator>> UpdateAdmin(UpdateAdminDTO adminDTO, string id);
         // Task<(int, string, UsageAdminDTO?)> AddInstitutionAccess(string adminId, string institutionId);
     }
 }
